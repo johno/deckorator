472baf4425c2a03609a3a1be143788db0731ddc4
Jake Mays <jake.m.mays@gmail.com>
2015-12-23 14:06:57 -0700

    Add ability to decorate arrays or relations.

--- spec/deckorator_spec.rb
+++ spec/deckorator_spec.rb
@@ -5,25 +5,31 @@ describe Deckorator do
     expect(Deckorator::VERSION).not_to be nil
   end
 
-  describe '.decorate' do
+  describe '.deckorate' do
     context 'object is nil' do
-      it { expect(Deckorator.decorate(nil)).to be_nil }
+      it { expect(Deckorator.deckorate(nil)).to be_nil }
     end
     context 'object is plain ruby' do
       let(:post) { Post.new('Testing 1 2 3') }
-      it { expect(Deckorator.decorate(post)).to be_a PostDeckorator }
+      it { expect(Deckorator.deckorate(post)).to be_a PostDeckorator }
     end
     context 'object has decorator_class class method' do
       let(:comment) { Comment.new('Haters gonna hate.') }
-      it { expect(Deckorator.decorate(comment)).to be_a CommentDecorator }
+      it { expect(Deckorator.deckorate(comment)).to be_a CommentDecorator }
     end
     context 'object has decorator_class instance method' do
       let(:user) { User.new('bravely') }
-      it { expect(Deckorator.decorate(user)).to be_a UserDecorator }
+      it { expect(Deckorator.deckorate(user)).to be_a UserDecorator }
     end
     context 'object is an ActiveRecord object' do
       let(:blog) { Blog.new('Noah Caldwell-Gervais') }
-      it { expect(Deckorator.decorate(blog)).to be_a BlogDeckorator }
+      it { expect(Deckorator.deckorate(blog)).to be_a BlogDeckorator }
+    end
+    context 'object is an array' do
+      let(:obj_array) { [Post.new('First!'), Comment.new('Scrublord')] }
+      it { expect(Deckorator.deckorate(obj_array).first).to be_a PostDeckorator }
+      it { expect(Deckorator.deckorate(obj_array).last).to be_a CommentDecorator }
+      it { expect(Deckorator.deckorate(obj_array)).to be_a Array }
     end
   end
 end
